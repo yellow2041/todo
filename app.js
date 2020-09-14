19198527a795be4afede45f9f6ea709c761d87df
yellow2041 <yellow2041@naver.com>
2020-09-14 23:41:44 +0900

    feat: DB연결중
    - 아직 query실행시 에러남

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3001;
+const mysql = require('mysql2/promise');
+const session = require('express-session');
 
 const indexRouter = require('./controller/routes/index');
+
+app.locals.connection = mysql.createConnection({
+    host: '49.50.172.152',
+    user: 'jy',
+    password: '0000',
+    database: 'mydb'
+  });
+
+//app.locals.connection.connect();
+
+app.use(session({
+    secret: 'keyboard cat', 
+    resave: false,
+    saveUninitialized: true,
+  }));
 
 app.use('/',indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+module.exports = app;
--- controller/routes/index.js
+++ controller/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path')
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-router.get('/', function (req, res) {
+router.get('/', async (req, res) => {
+    const [row] = await req.app.locals.connection.query(
+        'SELECT name FROM user WHERE userid = 1'
+    );
+    res.send(row)
     res.sendFile(path.join(__dirname, "../../view/index.html"));
 });
+router.post('/',async (req, res,err)=>{
+    if(!req.session.userid){
+        req.session.userid=1;
+        const [row] = await req.app.locals.connection.query(
+            'SELECT name FROM user WHERE id = 1'
+        );
+        req.session.name=row;
+    }
+    console.log(req.session);
+    res.send(req.session);
+});
+
 
 module.exports = router;
